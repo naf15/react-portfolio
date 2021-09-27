dd14fa3d2ed310b7269922b6723d89f651517ca1
Nafis Rahman <nmr278@nyu.edu>
2021-09-27 09:46:15 -0400

    Added browser routes to app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route } from "react-router-dom";
+import Header from './components/Header';
+import AboutMe from './components/AboutMe';
+import Portfolio from './components/Portfolio';
+import ContactMe  from './components/ContactMe';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header></Header>
+      <main>
+        <Route exact path="/">
+          <AboutMe></AboutMe>
+        </Route>
+        <Route exact path="/porfolio">
+          <Porfolio></Porfolio>
+        </Route>
+        <Route exact path="/contact-me">
+          <ContactMe></ContactMe>
+        </Route>
+      </main>
+    </BrowserRouter>
   );
 }
 
